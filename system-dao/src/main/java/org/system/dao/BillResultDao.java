1458cb4d9c9c6e517d2939e70a4b2b0e9d66d321
midouy <837291157@qq.com>
2016-02-19 15:31:30 +0800

    nothing

--- system-dao/src/main/java/org/system/dao/BillResultDao.java
+++ system-dao/src/main/java/org/system/dao/BillResultDao.java
@@ -1,8 +1,14 @@
 package org.system.dao;
 
+import org.system.domain.account.BillResult;
+
+import java.util.List;
+
 /**
  * Created by wangyanming on 2016/2/4.
  */
 public interface BillResultDao
 {
+    Integer addBillResult(BillResult billResult);
+    List<BillResult> listAllResultHistory();
 }
