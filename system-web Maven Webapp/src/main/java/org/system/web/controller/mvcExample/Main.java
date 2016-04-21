3f4158220fc565386c544218b890aa5a6efafb30
midouy <837291157@qq.com>
2016-04-21 15:43:52 +0800

    test

--- system-web Maven Webapp/src/main/java/org/system/web/controller/mvcExample/Main.java
+++ system-web Maven Webapp/src/main/java/org/system/web/controller/mvcExample/Main.java
@@ -1,15 +1,17 @@
 package org.system.web.controller.mvcExample;
 
+import bit.wym.commons.date.DateUtils;
 import bit.wym.commons.util.DataShower;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.system.common.response.ResponseUtil;
 import org.system.web.controller.mvcExample.beansForExample.Person;
+import org.system.web.controller.mvcExample.beansForExample.PersonList;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by wangyanming on 2015/8/18.
@@ -39,11 +41,23 @@ public class Main
 		return "test";
 	}
 
-	public static void main(String[] args)
-	{
-		System.out.println("Hello world");
+
+	/**
+	 *正常的数据传递
+	 */
+	@RequestMapping("/test/normal")
+	@ResponseBody
+	public Map<String, Object> TestNormal(HttpServletRequest request, String name, int age){
+		System.out.println(request.toString());
+		System.out.println("name : " + name);
+		System.out.println("age : " + age);
+		return ResponseUtil.successResult("testNormal run");
 	}
 
+
+	/**
+	 * 通过JSON字符串的形式传递一个数组
+	 */
 	@RequestMapping(value = "test/array")
 	@ResponseBody
 	public void TestArray(@RequestBody List<Person> users, HttpServletRequest request)
@@ -51,5 +65,59 @@ public class Main
 		System.out.println("testArray");
 		DataShower.show(users);
 	}
+
+	/**
+	 * 封装成一个对象
+	 */
+	@RequestMapping(value = "testBean")
+	@ResponseBody
+	public Map<String, Object> testBean(HttpServletRequest request, Model model, @ModelAttribute Person person)
+	{
+		try
+		{
+			System.out.println("REQUEST Getting : testBean");
+			System.out.println(person);
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return ResponseUtil.failResult(" Request failed : " + "testBean");
+	}
+
+	/**
+	 * 封装成一个复杂对象
+	 * */
+	@RequestMapping(value = "complex")
+	@ResponseBody
+	public Map<String, Object> complex(HttpServletRequest request, Model model, PersonList users)
+	{
+		try
+		{
+			System.out.println("REQUEST Getting [ "+ DateUtils.getCurrentFormatDateTime()+" ] : complex ");
+			System.out.println("name : " + users.getTitle());
+			System.out.println("age : " + users.getAge());
+			DataShower.show(users.getPersons());
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return ResponseUtil.failResult(" Request failed : " + "complex");
+	}
+
+	@RequestMapping(value = "testNothing")
+	@ResponseBody
+	public Map<String, Object> testNothing(HttpServletRequest request, Model model)
+	{
+		try
+		{
+			System.out.println("REQUEST Getting [ " + DateUtils.getCurrentFormatDateTime() + " ] : testNothing");
+			System.out.println("just test nothing");
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return ResponseUtil.failResult(" Request failed : " + "testNothing");
+	}
+
 
 }
--- system-web Maven Webapp/src/main/webapp/WEB-INF/jsp/main/test.jsp
+++ system-web Maven Webapp/src/main/webapp/WEB-INF/jsp/main/test.jsp
@@ -14,16 +14,25 @@
     <title>main test !</title>
 </head>
 <body>
+<div>
+    <br/><br/><span>SpringMVC 数据传递 : </span><br/><br/>
     <input type="button" value="testArray" onclick="testArray();">
+    <input type="button" value="testNormal" onclick="testNormal();">
+    <input type="button" value="testBean" onclick="testBean();">
+    <input type="button" value="testComplex" onclick="testComplex();">
+</div>
+
 </body>
 <script type="text/javascript" src="${ctx}/js/jq_183.js"></script>
 <script>
+
+    var saveDataAry=[];
+    var data1={"name":"test","address":"gz"};
+    var data2={"name":"ququ","address":"gr"};
+    saveDataAry.push(data1);
+    saveDataAry.push(data2);
+
     function testArray(){
-        var saveDataAry=[];
-        var data1={"name":"test","address":"gz"};
-        var data2={"name":"ququ","address":"gr"};
-        saveDataAry.push(data1);
-        saveDataAry.push(data2);
         $.ajax({
             type:"POST",
             url:"${ctx}/main/test/array",
@@ -35,5 +44,74 @@
             }
         });
     }
+
+    function testNormal() {
+        var data = {}; //使用自己定义的JS对象作为ajax的传输参数
+        data["name"] = "sagelasy";
+        data["age"] = 2000;
+
+        $.ajax({
+           url: "${ctx}/main/test/normal",
+            dataType:"json",
+            contentType:'application/json; charset=UTF-8',
+            data:data,
+//            {
+//                name : "sagelasy",
+//                age: 2000
+//            },
+            success:function(data){
+                alert(data.message);
+            }
+        });
+    }
+
+    function testBean(){
+        $.ajax({
+            url: "${ctx}/main/testBean",
+            dataType: "json",
+            data: {
+                name : "sagelasy",
+                address: 'storm'
+            },
+            success: function (data) {
+                alert(data.message);
+            }
+        });
+    }
+
+    function testComplex() {
+        var data = {}; //使用自己定义的JS对象作为ajax的传输参数
+        data["title"] = "sagelasy";
+        data["age"] = 2000;
+
+        for(var i = 0; i<saveDataAry.length; i++){
+            data["persons["+i+"].name"] = saveDataAry[i].name;
+            data["persons["+i+"].address"] = saveDataAry[i].address;
+        }
+
+        /**
+         * 需要前端自行拼接成以下形式
+         name:sagelasy
+         age:2000
+         persons[0].name:test
+         persons[0].address:gz
+         persons[1].name:ququ
+         persons[1].address:gr
+         */
+        $.ajax({
+            url: "${ctx}/main/complex",
+            dataType:"json",
+            data:data,
+//            {
+//                name : "sagelasy",
+//                age: 2000,
+//                persons : saveDataAry
+//            },
+            success:function(data){
+                alert(data.message);
+            }
+        });
+    }
+
 </script>
 </html>
