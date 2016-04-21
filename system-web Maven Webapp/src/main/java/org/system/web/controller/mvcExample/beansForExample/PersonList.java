efd3f85229f0b36702205f7bd9c190602dcc6018
midouy <837291157@qq.com>
2016-04-21 15:44:36 +0800

    test

---
+++ system-web Maven Webapp/src/main/java/org/system/web/controller/mvcExample/beansForExample/PersonList.java
@@ -0,0 +1,45 @@
+package org.system.web.controller.mvcExample.beansForExample;
+
+import java.util.List;
+
+/**
+ * Created by wangyanming on 2016/4/21.
+ */
+public class PersonList
+{
+	private List<Person> persons;
+
+	private String title;
+
+	private int age;
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
+
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+
+	public List<Person> getPersons()
+	{
+		return persons;
+	}
+
+	public void setPersons(List<Person> persons)
+	{
+		this.persons = persons;
+	}
+}
