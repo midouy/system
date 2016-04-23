514a944fd3c2b9e0e521aa87582655cd5a05885e
midouy <837291157@qq.com>
2016-04-23 12:11:47 +0800

    delete eclipse files

--- system-commons/src/main/java/org/system/common/context/account/AccountContext.java
+++ system-commons/src/main/java/org/system/common/context/account/AccountContext.java
@@ -1,5 +1,12 @@
 package org.system.common.context.account;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
 /**
  * Created by wangyanming on 2016/2/3.
  */
@@ -9,4 +16,5 @@ public class AccountContext
     public static String NAME_LRH = "刘偌含";
     public static String NAME_CX = "陈茜";
     public static String NAME_ZXW = "张晓巍";
+
 }
