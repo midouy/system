b4954f1d2355e88c22f6f4a9c379e293a952c65c
midouy <837291157@qq.com>
2016-12-29 20:41:54 +0800

    spring task test

--- system-web Maven Webapp/src/main/java/org/system/web/task/SpringTaskTestService.java
+++ system-web Maven Webapp/src/main/java/org/system/web/task/SpringTaskTestService.java
@@ -1,5 +1,7 @@
 package org.system.web.task;
 
+import  org.slf4j.Logger;
+import  org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +15,12 @@ import java.util.Date;
 @Lazy(value=false)
 public class SpringTaskTestService
 {
+	private Logger logger = LoggerFactory.getLogger(SpringTaskTestService.class);
 	public void execute()throws Exception{
 		String time = DateFormat.getDateTimeInstance().format(new Date());
 		System.out.println("定时器触发打印"+time);
-		System.out.println(Thread.currentThread());
+//		System.out.println(Thread.currentThread());
+		logger.info(Thread.currentThread().toString());
 		long start = System.currentTimeMillis();
 		while (System.currentTimeMillis()-start<5*1000){
 		}
